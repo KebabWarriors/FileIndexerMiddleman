550b31cdebe93bf0d86496aacc2c7b297984135e
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-26 19:12:32 +0200

    Fixed .editorconfig.

--- file_indexer_middleman/middleman.py
+++ file_indexer_middleman/middleman.py
@@ -2,8 +2,8 @@ import rpyc
 from py4j.java_gateway import JavaGateway
 
 class Middleman(rpyc.Service):
-    def on_connect(self, conn):
-        pass
+  def on_connect(self, conn):
+    pass
 
-    def on_disconnect(self, conn):
-        pass
+  def on_disconnect(self, conn):
+    pass
